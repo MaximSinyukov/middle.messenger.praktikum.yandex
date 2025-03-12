636d0b787750b8136c228c33af6fc8a757031f34
Maxim Sinyukov <verero1173@yandex.ru>
2025-03-12 19:55:00 +0300

    feat: create base-structure

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
 import './index.scss';
-import javascriptLogo from 'svg/javascript.svg';
-import viteLogo from 'svg/vite.svg';
-import { setupCounter } from 'components/counter.js';
+import App from './App.js';
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`;
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new App();
 
-setupCounter(document.querySelector('#counter'));
+  app.render();
+});
