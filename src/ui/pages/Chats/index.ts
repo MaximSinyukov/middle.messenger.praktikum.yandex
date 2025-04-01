ebb6eed82309a9de3e5c11aab61e3dc43809185f
Maxim Sinyukov <verero1173@yandex.ru>
2025-04-01 22:23:45 +0300

    feat: add styles for messages-container

--- src/ui/pages/Chats/index.ts
+++ src/ui/pages/Chats/index.ts
@@ -3,6 +3,8 @@ import ChatsTemplate from "./index.hbs?raw";
 import chatsComponents from "./components/index";
 
 import arrowRightIcon from "svg/arrow-right.svg";
+import chatsList from "./utils/constants/chats-list";
+import selectedChat from "./utils/constants/selected-chat";
 
 const ChatsData = {
   button: {
@@ -11,79 +13,8 @@ const ChatsData = {
     title: "Профиль",
     iconSrc: arrowRightIcon,
   },
-  contacts: [
-    {
-      chatImage: "",
-      chatName: "Жмышенко В.А.",
-      lastMessage: {
-        text: "Слышь Ты",
-        sender: "1",
-        time: "10:48",
-      },
-      unreadMessages: 0,
-    },
-    {
-      chatImage: "",
-      chatName:
-        "Антон Палыч Шпак Бяк бяк бяк бяк бяк бяк бяк бяк бяк бяк бяк бяк бяк",
-      lastMessage: {
-        text: "Я ни понял ты хто такой?",
-        sender: "2",
-        time: "09:01",
-      },
-      unreadMessages: 1,
-    },
-    {
-      chatImage: "",
-      chatName: "Хихихихи",
-      lastMessage: {
-        text: "Даб даб даб даб даб даб даб даб даб даб даб даб даб даб даб даб Даб даб даб даб даб даб даб даб даб даб даб даб даб даб даб даб",
-        sender: "3",
-        time: "Ср",
-      },
-      unreadMessages: "9+",
-    },
-    {
-      chatImage: "",
-      chatName: "Валерий Альбертович",
-      lastMessage: {
-        text: "Извинись.",
-        sender: "4",
-        time: "Вт",
-      },
-      unreadMessages: 2,
-    },
-    {
-      chatImage: "",
-      chatName: "конфа Боровов",
-      lastMessage: {
-        text: "Якупиджаку сегодня готовил, хочешь? Каво",
-        sender: "5",
-        time: "20 мая 1999",
-      },
-      unreadMessages: 0,
-    },
-    {
-      chatImage: "",
-      chatName: "Шмурдяк",
-      lastMessage: {
-        text: "яяяяяяяяяяяя",
-        sender: "65",
-        time: "06 мая 1999",
-      },
-      unreadMessages: "9+",
-    },
-    {
-      chatImage: "",
-      chatName: "к121212",
-      lastMessage: {
-        text: "аааааааааааааааааааа",
-        sender: "53",
-        time: "01 мая 1999",
-      },
-      unreadMessages: 0,
-    },
-  ],
+  chatsList,
+  selectedChat,
 };
 
 export default class Chats extends RenderComponent {
